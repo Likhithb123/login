daff05a50273e5803fa4693948960880d393e0e9
Likhith Boola <137615143+Likhithb123@users.noreply.github.com>
2024-06-10 14:06:20 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,135 @@
+/*const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const knex = require('knex');
+
+const app = express();
+const port = 3000;
+
+let intialPath = path.join(__dirname,"public");
+
+app.use(bodyParser.json());
+app.use(express.static(intialPath));
+
+app.get('/',(req,res) => {
+  res.sendFile(path.join(intialPath,"index.html"));
+})
+app.get('/login',(req,res) => {
+  res.sendFile(path.join(intialPath,"login.html"));
+})
+app.get('/register',(req,res) => {
+  res.sendFile(path.join(intialPath,"register.html"));
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});*/
+
+
+/*const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const knex = require('knex');
+
+const app = express();
+const port = 3000;
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+// Route to serve the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'register.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});*/
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'Likhith@720',
+        database: 'loginform'
+    }
+})
+
+const app = express();
+
+let intialPath = path.join(__dirname, "public");
+
+app.use(bodyParser.json());
+app.use(express.static(intialPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(intialPath, "index.html"));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(intialPath, "login.html"));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(intialPath, "register.html"));
+})
+
+app.post('/register-user', (req, res) => {
+    const { name, email, password } = req.body;
+
+    if(!name.length || !email.length || !password.length){
+        res.json('fill all the fields');
+    } else{
+        db("users").insert({
+            name: name,
+            email: email,
+            password: password
+        })
+        .returning(["name", "email"])
+        .then(data => {
+            res.json(data[0])
+        })
+        .catch(err => {
+            if(err.detail.includes('already exists')){
+                res.json('email already exists');
+            }
+        })
+    }
+})
+
+app.post('/login-user', (req, res) => {
+    const { email, password } = req.body;
+
+    db.select('name', 'email')
+    .from('users')
+    .where({
+        email: email,
+        password: password
+    })
+    .then(data => {
+        if(data.length){
+            res.json(data[0]);
+        } else{
+            res.json('email or password is incorrect');
+        }
+    })
+})
+
+app.listen(3002, (req, res) => {
+    console.log('listening on port 3002......')
+}) 
